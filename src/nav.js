508009fe1eb692cda9eeff91a3f6b5ea470d50fa
HasnatSN <hasnat.kontakt@gmail.com>
2022-04-27 19:39:09 +0200

    rewriting the code with a factory function

--- src/nav.js
+++ src/nav.js
@@ -3,23 +3,44 @@ class Project {
     this.projectsArray = [];
   }
 
-  newProjectFolder() {
-    let newButton = document.createElement("button");
-    newButton.setAttribute("data-index", this.projectsArray.length);
-    newButton.innerHTML =
-      '<div><i class="fa-solid fa-folder"></i> Project X</div><button class="remove"><i class="fa-solid fa-xmark"></i></button>';
-    this.projectsArray.push(newButton);
-    console.log(this.projectsArray);
-    return newButton;
+  button() {
+      
   }
 
-  addProject() {
-    const addProjectBtn = document.querySelector(".add-project");
-    addProjectBtn.addEventListener("click", () => {
-      const nav = document.querySelector("nav");
-      nav.appendChild(this.newProjectFolder());
-    });
-  }
+
 }
 
 export { Project };
+
+
+
+
+
+
+
+
+// newProjectFolder() {
+//     let newButton = document.createElement("button");
+//     newButton.setAttribute("data-index", this.projectsArray.length);
+//     newButton.innerHTML =
+//       '<div><i class="fa-solid fa-folder"></i> Project X</div><button class="remove"><i class="fa-solid fa-xmark"></i></button>';
+//     this.projectsArray.push(newButton);
+//     console.log(this.projectsArray);
+//     this.removeFolder(newButton);
+//     return newButton;
+//   }
+
+//   addProject() {
+//     const addProjectBtn = document.querySelector(".add-project");
+//     addProjectBtn.addEventListener("click", () => {
+//       const nav = document.querySelector("nav");
+//       nav.appendChild(this.newProjectFolder());
+//     });
+//   }
+
+//   removeFolder(button) {
+//     console.log(button);
+//     //   button.addEventListener("click", () => {
+          
+//     //   })
+//   }
