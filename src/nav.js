344b317f4383bf40d41d8274944f6c4a2625481e
HasnatSN <hasnat.kontakt@gmail.com>
2022-04-28 22:00:24 +0200

    nav project feature works now, custom name is still in progress

--- src/nav.js
+++ src/nav.js
@@ -5,7 +5,7 @@ class Project {
 
   createProjectButton() {
     let button = document.createElement("button");
-    button.setAttribute("data-index", this.projectsArray);
+    button.setAttribute("data-index", this.projectsArray.length);
     return button;
   }
 
@@ -15,17 +15,42 @@ class Project {
     return divWithI;
   }
 
-  createFolderNameDiv(folderName) {
+  createFolderNameDiv() {
     let folderNameDiv = document.createElement("div");
     folderNameDiv.classList.add("project-name");
-    folderNameDiv.textContent = folderName;
+    folderNameDiv.textContent = `${this.projectsArray.length}`;
     return folderNameDiv;
+  }
+
+  removeProjectBtnFromArray(index) {
+    this.projectsArray.splice(index, 1);
+    console.log(this.projectsArray);
+  }
+
+  removeProjectBtnFromDom(index) {
+    const nav = document.querySelector("nav");
+    let childToBeRemoved = nav.childNodes[index + 11];
+    console.log(childToBeRemoved)
+    childToBeRemoved.parentNode.removeChild(childToBeRemoved);
+  }
+
+  removeProjectBtn(removeBtn) {
+    removeBtn.addEventListener("click", () => {
+      let index = this.projectsArray.findIndex(
+        (button) => removeBtn.dataset.index == button.dataset.index
+      );
+      this.removeProjectBtnFromArray(index);
+      this.removeProjectBtnFromDom(index);
+    });
   }
 
   createRemoveBtn() {
     let removeBtn = document.createElement("button");
+    removeBtn.setAttribute("data-index", this.projectsArray.length);
     removeBtn.innerHTML =
       '<button class="remove"><i class="fa-solid fa-xmark"></i></button>';
+
+    this.removeProjectBtn(removeBtn);
     return removeBtn;
   }
 
@@ -39,7 +64,7 @@ class Project {
     // create all the elements for the custom project button
     this.button = this.createProjectButton();
     this.divWithI = this.createDivWithI();
-    this.folderNameDiv = this.createFolderNameDiv(prompt("Folder name?"));
+    this.folderNameDiv = this.createFolderNameDiv();
     this.removeBtn = this.createRemoveBtn();
 
     // append all the children elements to the parent button
